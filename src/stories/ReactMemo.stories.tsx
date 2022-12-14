516b40f3d19a3dcbee095829d798aa87e14bf4a6
Paskinho <v.paskanov@gmail.com>
2022-12-14 20:27:02 +0300

    end 17 lesson

--- src/stories/ReactMemo.stories.tsx
+++ src/stories/ReactMemo.stories.tsx
@@ -8,14 +8,21 @@ export default {
 const NewMessageCounter = (props:{count: number}) =>  {
     return <div> {props.count}</div>
 }
-const Users = (props: {users: Array<string>}) =>  {
-    return <div> {props.users.map((u,i)=> <div key={{i}}>{u}</div>)}
+const UsersSecret = (props: {users: Array<string>}) =>  {
+    return <div> {props.users.map((u,i)=> <div key={i}>{u}</div>)}
     </div>
 }
+
+const Users = React.memo(UsersSecret);
 
 export const Example1 = () => {
     const [counter, setCounter] = useState(0)
     const [users, setUsers] = useState (["Dimych", "Valera", "Artem"])
+
+    const addUser = ()=> {
+        const newUser = [...users, "Svetlana" + new Date().getTime()]
+        setUsers(newUser);
+    }
 
 
     return <>
