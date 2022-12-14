4a0060cf7c2c0345d6855f3340a8989eba8e4642
Paskinho <v.paskanov@gmail.com>
2022-12-14 20:17:31 +0300

    17 lesson add Counter and Users

--- src/stories/ReactMemo.stories.tsx
+++ src/stories/ReactMemo.stories.tsx
@@ -1,3 +1,28 @@
+import React, {useState} from "react";
+
 export default {
     title: "React.memo demo"
 }
+
+
+const NewMessageCounter = (props:{count: number}) =>  {
+    return <div> {props.count}</div>
+}
+const Users = (props: {users: Array<string>}) =>  {
+    return <div> {props.users.map((u,i)=> <div key={{i}}>{u}</div>)}
+    </div>
+}
+
+export const Example1 = () => {
+    const [counter, setCounter] = useState(0)
+    const [users, setUsers] = useState (["Dimych", "Valera", "Artem"])
+
+
+    return <>
+        <button onClick={() => setCounter(counter + 1)}> + </button>
+    <NewMessageCounter count={counter}/>
+    <Users users={users}/>
+    </>
+}
+
+
