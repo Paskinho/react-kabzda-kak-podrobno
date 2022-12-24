45acee5d9f83cb2cbe801ed49896d07029005de5
Paskinho <v.paskanov@gmail.com>
2022-12-24 14:45:51 +0300

    18 lesson add UseMemo.stories.tsx

---
+++ src/stories/UseMemo.stories.tsx
@@ -0,0 +1,28 @@
+import React, {useState} from 'react'
+
+export default {
+    title: 'useMemo',
+}
+
+    export const Example1 = () => {
+
+    const [a, setA] = useState<number>(0)
+    const [b, setB] = useState<number>(0)
+
+
+    return <>
+<input value={a} onChange={(e)=> setA(Number(e.currentTarget.value))}/>
+<input value={b} onChange={(e)=> setB(+(e.currentTarget.value))}/>
+        <hr>
+        <div>
+            Result for a: {}
+        </div>
+            <div>
+                Result for b: {}
+            </div>
+        </hr>
+
+
+    </>
+    }
+
