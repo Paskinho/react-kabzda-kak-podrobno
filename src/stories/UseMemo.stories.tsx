44cf25295abcfc86175c22c46448c2ed16dc1e99
Paskinho <v.paskanov@gmail.com>
2022-12-24 14:56:17 +0300

    18 lesson add factorial

--- src/stories/UseMemo.stories.tsx
+++ src/stories/UseMemo.stories.tsx
@@ -9,19 +9,28 @@ export default {
     const [a, setA] = useState<number>(0)
     const [b, setB] = useState<number>(0)
 
+        let resultA = 1
+        let resultB = 1
+
+        for (let i=1; i <=a; i++) {
+            resultA = resultA * 1
+        }
+
+        for (let i=1; i <=b; i++) {
+            resultB = resultB * 1
+        }
+
 
     return <>
 <input value={a} onChange={(e)=> setA(Number(e.currentTarget.value))}/>
 <input value={b} onChange={(e)=> setB(+(e.currentTarget.value))}/>
-        <hr>
+<hr/>
         <div>
             Result for a: {}
         </div>
             <div>
                 Result for b: {}
             </div>
-        </hr>
-
 
     </>
     }
