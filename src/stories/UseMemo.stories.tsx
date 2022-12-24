088d11d248eadc48f7d819cdc5a78da0a8de1866
Paskinho <v.paskanov@gmail.com>
2022-12-24 15:16:54 +0300

    18 lesson add UseMemo

--- src/stories/UseMemo.stories.tsx
+++ src/stories/UseMemo.stories.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useMemo, useState} from 'react'
 
 export default {
     title: 'useMemo',
@@ -12,12 +12,16 @@ export default {
         let resultA = 1
         let resultB = 1
 
-        for (let i=1; i <=a; i++) {
-            resultA = resultA * 1
-        }
+        resultA = useMemo(()=>{
+            let tempResultA = 1;
+            for (let i=1; i <=a; i++) {
+                resultA = resultA * i
+            }
+            return tempResultA
+        }, [a])
 
         for (let i=1; i <=b; i++) {
-            resultB = resultB * 1
+            resultB = resultB * i
         }
 
 
@@ -26,10 +30,10 @@ export default {
 <input value={b} onChange={(e)=> setB(+(e.currentTarget.value))}/>
 <hr/>
         <div>
-            Result for a: {}
+            Result for a: {resultA}
         </div>
-            <div>
-                Result for b: {}
+            <div>B
+                Result for b: {resultB}
             </div>
 
     </>
