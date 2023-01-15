118d27cb277206ef506a465537870f8f2a3d21fa
Paskinho <v.paskanov@gmail.com>
2023-01-15 16:26:53 +0300

    19 lesson add LikeUseCallback

--- src/stories/UseMemo.stories.tsx
+++ src/stories/UseMemo.stories.tsx
@@ -66,3 +66,25 @@ return newArray
         <Users users={users}/>
     </>
 }
+
+
+export const LikeUseCallback = () => {
+    const [counter, setCounter] = useState(0)
+    const [users, setUsers] = useState (["Dimych", "Valera", "Artem"])
+
+    const newArray = useMemo(()=> {
+        const newArray = users.filter(u=> u.toLowerCase().indexOf('a') > -1)
+        return newArray
+    },[users])
+
+    const addUser = ()=> {
+        const newUsers = [...users,'Sveta' + new Date().getTime()];
+        setUsers(newUsers)
+    }
+
+    return <>
+        <button onClick={() => setCounter(counter + 1)}> + </button>
+        <button onClick={() => addUser()}> add user </button>
+        <Users users={users}/>
+    </>
+}
