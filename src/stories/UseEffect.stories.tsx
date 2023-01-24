f3cff642b9ccfc2bb7bdd50f2ed06de1fc091858
Paskinho <v.paskanov@gmail.com>
2023-01-24 18:22:01 +0300

    end 21 lesson (add UseEffect.stories.tsx)

---
+++ src/stories/UseEffect.stories.tsx
@@ -0,0 +1,40 @@
+import React, {useEffect, useState} from 'react'
+
+export default {
+    title: "useEffect demo"
+}
+
+export const SimpleExample = () => {
+    const [fake,setFake] = useState(1)
+const [counter,setCounter] = useState(1)
+
+
+console.log("SimpleExample");
+
+useEffect(()=> {
+    console.log("useEffect every render");
+    document.title = counter.toString()
+})
+
+    useEffect(()=> {
+        console.log("useEffect only first render (componentDidMount)");
+        document.title = counter.toString()
+    })
+
+    useEffect(()=> {
+        console.log("useEffect only first render and every counter change");
+        document.title = counter.toString()
+    })
+
+    return <>
+    Hello,{counter} {fake}
+        <button onClick={() => {
+        setFake(fake+1)}
+        }>fake+</button>
+        <button onClick={() => {
+            setCounter(counter+1)}
+        }>counter+</button>
+    </>
+
+
+}
